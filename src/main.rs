1fa6ee64f5a728586ae35897dfb06e310cb45a67
Ömer Sinan Ağacan <omeragacan@gmail.com>
2022-09-04 18:22:13 +0200

    Handle items with more than one derives

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use fxhash::FxHashSet;
+use fxhash::FxHashMap;
+use proc_macro2::Span;
 use quote::{quote, ToTokens};
 
 fn main() {
@@ -10,44 +11,83 @@ fn main() {
 }
 
 fn process_file(file: &mut syn::File) {
-    let debug_trait_path_str = quote!(::core::fmt::Debug).to_string();
+    let mut item_derives: FxHashMap<usize, Vec<syn::Path>> = Default::default();
 
-    let mut removed_item_indices: FxHashSet<usize> = Default::default();
+    let old_items = std::mem::take(&mut file.items);
 
-    for (item_idx, item) in file.items.iter().enumerate() {
+    let mut last_non_derive_item: usize = 0;
+
+    // Collect automatic derives
+    for (item_idx, item) in old_items.iter().enumerate() {
         if let syn::Item::Impl(impl_) = item {
-            if let Some((_, trait_path, _)) = &impl_.trait_ {
-                // TODO: Add all `automatically_derived` impls
-                if trait_path.into_token_stream().to_string() == debug_trait_path_str {
-                    removed_item_indices.insert(item_idx);
+            if is_automatically_derived(&impl_.attrs) {
+                if let Some((_, trait_path, _)) = &impl_.trait_ {
+                    item_derives
+                        .entry(last_non_derive_item)
+                        .or_default()
+                        .push(trait_path.clone());
                 }
             }
+        } else {
+            last_non_derive_item = item_idx;
         }
     }
 
-    // Drop removed items, add derive attributes
-    let old_items = std::mem::take(&mut file.items);
-    file.items = old_items
+    // Turn automatic derives into derive attributes, remove automatic derives
+    let new_items: Vec<syn::Item> = old_items
         .into_iter()
         .enumerate()
-        .filter_map(|(item_idx, mut item)| {
-            if removed_item_indices.contains(&item_idx) {
-                return None;
-            }
-
-            if removed_item_indices.contains(&(item_idx + 1)) {
-                match item {
-                    syn::Item::Enum(ref mut enum_item) => {
-                        enum_item.attrs.push(syn::parse_quote!(#[derive(Debug)]));
-                    }
-                    syn::Item::Struct(ref mut struct_item) => {
-                        struct_item.attrs.push(syn::parse_quote!(#[derive(Debug)]));
-                    }
-                    _ => panic!(),
+        .filter_map(|(item_idx, mut item)| match item_derives.get(&item_idx) {
+            Some(derives) => match item {
+                syn::Item::Enum(ref mut enum_item) => {
+                    enum_item.attrs.push(make_item_derive_attribute(derives));
+                    Some(item)
                 }
+                syn::Item::Struct(ref mut struct_item) => {
+                    struct_item.attrs.push(make_item_derive_attribute(derives));
+                    Some(item)
+                }
+                _ => panic!(),
+            },
+            None => {
+                if let syn::Item::Impl(impl_) = &item {
+                    if is_automatically_derived(&impl_.attrs) {
+                        return None;
+                    }
+                }
+                Some(item)
             }
-
-            Some(item)
         })
         .collect();
+
+    file.items = new_items;
+}
+
+fn is_automatically_derived(attrs: &[syn::Attribute]) -> bool {
+    let attr_str = quote!(automatically_derived).to_string();
+    attrs
+        .iter()
+        .any(|attr| (&attr.path).into_token_stream().to_string() == attr_str)
+}
+
+fn make_item_derive_attribute(derives: &[syn::Path]) -> syn::Attribute {
+    syn::Attribute {
+        pound_token: syn::token::Pound {
+            spans: [Span::call_site()],
+        },
+        style: syn::AttrStyle::Outer,
+        bracket_token: syn::token::Bracket {
+            span: Span::call_site(),
+        },
+        path: syn::Path {
+            leading_colon: None,
+            segments: syn::punctuated::Punctuated::from_iter(std::iter::once::<syn::PathSegment>(
+                syn::PathSegment {
+                    ident: syn::Ident::new("derive", Span::call_site()),
+                    arguments: Default::default(),
+                },
+            )),
+        },
+        tokens: quote!((#(#derives),*)),
+    }
 }
